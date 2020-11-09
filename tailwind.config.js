9d6f7a7dc0805f4e0c436948f981dc7d8c7891c9
Jayan Ratna <jayan@blutui.com>
2020-11-10 10:39:10 +1300

    feat: add about us page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,41 @@ module.exports = {
     ]
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyber-yellow': {
+          50: '#FFFDF2',
+          100: '#FFFBE6',
+          200: '#FFF4C0',
+          300: '#FEED9B',
+          400: '#FEE04F',
+          500: '#FDD204',
+          600: '#E4BD04',
+          700: '#987E02',
+          800: '#725F02',
+          900: '#4C3F01',
+        },
+      },
+      fontFamily: {
+        effra: [
+          'effra',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ]
+      }
+    },
   },
   variants: {},
   plugins: [],
